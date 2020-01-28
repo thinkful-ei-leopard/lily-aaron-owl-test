4047321a78a22fa85f3a4e1e1347b6ea7c29a2c4
Aaron Turcios <aar.turcios@gmail.com>
2020-01-28 11:37:23 -0800

    commit issues recommiting

---
+++ src/ParticipantList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import 
+
+const ParticipantList = ({props}) => {
+    return (
+        <div className="part-list">
+        {participants.map(props.participants => )} 
+    )
+}
